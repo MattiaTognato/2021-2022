c85d9a5a10aea4def8bae77c2b4fcc2aa875e5ea
Matwy <mattiabasket2004@gmail.com>
2022-01-10 01:01:49 +0100

    ts

--- 9.TypeScript prova/prove.js
+++ 9.TypeScript prova/prove.js
@@ -1,1 +1,35 @@
-console.log("Hello world");
+"use strict";
+exports.__esModule = true;
+var readline = require("readline");
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+process.stdin.on('keypress', function (str, key) {
+    if (key.ctrl && key.name === 'c') {
+        process.exit();
+    }
+    checkKey(key);
+});
+console.log('Premere le freciette direzionali');
+console.log('^C per uscire');
+var contatore = 0;
+var contatoreSpazi = 0;
+function checkKey(k) {
+    if (k.name == 'up') {
+        contatore++;
+    }
+    else if (k.name == 'down') {
+        contatore--;
+    }
+    else if (k.name == 'right') {
+        contatoreSpazi--;
+    }
+    else if (k.name == 'left') {
+        contatoreSpazi--;
+    }
+    var res;
+    res = contatore.toString();
+    for (var i = 0; i < contatoreSpazi; i++) {
+        console.log(" ");
+    }
+    console.log(res);
+}
--- 9.TypeScript prova/prove.ts
+++ 9.TypeScript prova/prove.ts
@@ -1,1 +1,39 @@
-console.log("Hello world")
+import * as readline from 'readline';
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+process.stdin.on('keypress', (str, key) => {
+    if (key.ctrl && key.name === 'c') 
+    {
+        process.exit();
+    }
+    checkKey(key)
+});
+
+
+console.log('Premere le freciette direzionali');
+console.log('^C per uscire');
+
+var contatore:number = 0;
+var contatoreSpazi:number = 0;
+
+function checkKey(k):void {
+    if (k.name == 'up') {
+        contatore++;
+    }
+    else if (k.name == 'down'){
+        contatore--;
+    } 
+    else if (k.name == 'right'){
+        contatoreSpazi--;
+    } 
+    else if (k.name == 'left'){
+        contatoreSpazi--;
+    }
+    var res:string;
+    res = contatore.toString();
+    for(var i = 0; i < contatoreSpazi; i++)
+    {
+        console.log(" ");
+    }
+    console.log(res);    
+}
